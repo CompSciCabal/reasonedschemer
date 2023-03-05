7982acedb840063cf624cb7aa860e1beb52bbc3a
John Krasnay <john@krasnay.ca>
2023-03-05 17:46:48 -0500

    Add clj-kondo config

---
+++ .clj-kondo/hooks/corelogic.clj
@@ -0,0 +1,13 @@
+(ns hooks.corelogic
+  (:require
+    [clj-kondo.hooks-api :as api]))
+
+(defn run*
+  [{:keys [node]}]
+  (let [[_ args & body] (:children node)
+        syms (:children args)
+        let-bindings (mapcat (fn [sym] [sym (api/token-node 'nil)]) syms)]
+    {:node (api/list-node
+             (into [(api/token-node 'let)
+                    (api/vector-node let-bindings)]
+                   body))}))
