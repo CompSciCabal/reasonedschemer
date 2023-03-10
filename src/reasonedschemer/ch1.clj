c94588171adfd6e607939b6908def93a180526d5
John Krasnay <john@krasnay.ca>
2023-03-09 21:00:26 -0500

    Updates from 2023-03-09

--- src/reasonedschemer/ch1.clj
+++ src/reasonedschemer/ch1.clj
@@ -1,7 +1,7 @@
 ; vim: lispwords=run*,fresh:sw=2:
 (ns reasonedschemer.ch1
   (:require
-    [clojure.core.logic :refer [conde fresh fail run* s# u# == defrel]]
+    [clojure.core.logic :refer [conde fresh fail run* run s# u# == firsto resto lcons conso]]
     [reasonedschemer.util :refer [defrel]]))
 
 
@@ -197,4 +197,134 @@
 
 
 ;; Start on 2023-03-09 on frame 88
+
+(run* [x]
+  (conde
+    [(== 'olive x) u#]
+    [(== 'oil x)])) ; (oil)
+
+(run* [x y]
+  (conde
+    [(fresh [z]
+       (== 'lentil z))]
+    [(== x y)])) ; ([_0 _0] [_0 _1])
+
+(run* [x y]
+  (conde
+    [(== 'split x) (== 'pea y)]
+    [(== 'red x) (== 'bean y)]
+    [(== 'green x) (== 'lentil y)])) ; ([split pea] [red bean] [green lentil])
+
+;; Chapter 2
+
+(first '(grape raisin pear)) ; grape
+
+(first '(a c o r n)) ; a
+
+(run* [q]
+  (firsto '(a c o r n) q)) ; (a)
+
+(run* [q]
+  (firsto '(a c o r n) 'a)) ; (_0)
+
+(run* [r]
+  (fresh [x y]
+    (firsto (list r y) x)
+    (== 'pear x))) ; (pear)
+
+(cons
+  (first '(grape raisin pear))
+  (first '((a) (b) (c)))) ; (grape a)
+
+(run* [r]
+  (fresh [x y]
+    (firsto '(grape raisin pear) x)
+    (firsto '((a) (b) (c)) y)
+    (== (cons x y) r))) ; ((grape (a)))
+
+(rest '(grape raisin pear)) ; (raisin pear)
+
+(first (rest (rest '(a c o r n)))) ; o
+
+(run* [r]
+  (fresh [v]
+    (resto '(a c o r n) v)
+    (fresh [w]
+      (resto v w)
+      (firsto w r)))) ; (o)
+
+(defrel cdro
+  [l v]
+  (fresh [a]
+    (== (lcons a v) l)))
+
+(run* [v]
+  (cdro '(a b c) v)) ; ((b c))
+
+(cons
+  (rest '(grape raisin pear))
+  (first '((a) (b) (c)))) ; ((raisin pear) a)
+
+(run* [r]
+  (fresh [x y]
+    (resto '(grape raisin pear) x)
+    (firsto '((a) (b) (c)) y)
+    (== (lcons x y) r))) ; (((raisin pear) a))
+
+(run* [q]
+  (resto '(a c o r n) '(c o r n))) ; (_0)
+
+(run* [x]
+  (resto '(c o r n) (list x 'r 'n))) ; (o)
+
+(run* [l]
+  (fresh [x]
+    (resto l '(c o r n))
+    (firsto l x)
+    (== 'a x))) ; ((a c o r n))
+
+(run* [l]
+  (conso '(a b c) '(d e) l)) ; (((a b c) d e))
+
+(cons '(a b c) '(d e)) ; ((a b c) d e)
+
+(run* [x]
+  (conso x '(a b c) '(d a b c))) ; (d)
+
+(run* [r]
+  (fresh [x y z]
+    (== (list 'e 'a 'd x) r)
+    (conso y (list 'a z 'c) r))) ; ((e a d c))
+
+(run* [x]
+  (conso x (list 'a x 'c) (list 'd 'a x 'c))) ; (d)
+
+(run* [l]
+  (fresh [x]
+    (== (list 'd 'a x 'c) l)
+    (conso x (list 'a x 'c) l))) ; ((d a d c))
+
+(run* [l]
+  (fresh [x]
+    (conso x (list 'a x 'c) l)
+    (== (list 'd 'a x 'c) l))) ; ((d a d c))
+
+(defrel conso2
+  [v l x]
+  (conde
+    [(firsto x v)
+     (resto x l)])) ; #'reasonedschemer.ch1/conso2
+
+(run* [l v]
+  (conso2 v (list 'b 'c v) l)) ; ((a b c))
+; ([(_0 b c _0) _0])
+
+(defrel conso3
+  [v l x]
+  (== (lcons v l) x))
+
+(run* [l]
+  (conso3 'a (list 'b 'c) l)) ; ((a b c))
+
+;; Resume on 2023-03-16 on panel 28
 
